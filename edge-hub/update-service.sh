3318cdc78507111e1c504b98d70a81b8b67f7c88
Aleksandrs Livincovs <aleksandrs.livncovs@gmail.com>
2022-02-19 23:54:22 +0100

    - Added mosquitto container and config

--- edge-hub/update-service.sh
+++ edge-hub/update-service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /var/lib/mosquitto/config
+cp mosquitto/mosquitto.conf /var/lib/mosquitto/config/mosquitto.conf
 #docker-compose -f docker-compose.yml -p tplex-1 pull 
 #docker-compose -f docker-compose.yml -p tplex-1 up -d
 docker stack deploy --c docker-compose.yml tplex-1
